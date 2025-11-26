bf2752263bdd7ad80b3766dbddad63012ec6525a
nicolasaguilar2026-stack <nicolasaguilar2026@gmail.com>
2025-11-26 00:22:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// --- 1. IMPORTACIONES CORRECTAS (CDN) ---
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// --- 2. TU CONFIGURACIÓN (PROYECTO: simulador-a917b) ---
 const firebaseConfig = {
   apiKey: "AIzaSyDzvYIWuLW4e09HeEl8Fvs49yjAPXV3pH8",
   authDomain: "simulador-a917b.firebaseapp.com",
@@ -16,9 +14,11 @@ const firebaseConfig = {
   measurementId: "G-0EZD184L51"
 };
 
-// Initialize Firebase
+// --- 3. INICIALIZAR FIREBASE ---
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);        // <--- Esto faltaba y es vital para el login
+const db = getFirestore(app);     // <--- Esto faltaba para la base de datos
+
 
 // --- 2. LISTA DE CORREOS AUTORIZADOS Y DIFERENCIADOS ---
 
